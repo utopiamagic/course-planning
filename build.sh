4e62d201f8749c12b15b98f2bec6b1fa162a7ed7
David Ma <qingnian213@hotmail.com>
2017-02-22 15:56:42 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if command -v dot >/dev/null 2>&1; then
+  dot cpsc.src -ohello.png -Tpng
+else
+  echo 'GraphViz is not installed; please execute the following command: '
+  echo 'sudo dnf install graphviz -y'
+fi
