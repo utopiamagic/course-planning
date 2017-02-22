5b5ae8a2f789349751f63b9c86ad2c2a4e9ec6c5
David Ma <qingnian213@hotmail.com>
2017-02-22 15:59:46 -0800

    Removed an image

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if command -v dot >/dev/null 2>&1; then
-  dot cpsc.src -ohello.png -Tpng
+  dot cpsc.src -olatest.png -Tpng
 else
   echo 'GraphViz is not installed; please execute the following command: '
   echo 'sudo dnf install graphviz -y'
